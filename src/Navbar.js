0ac37d2e81f6bd4784a5f906eee4927f7651c84d
MOHAMED FARHUN <86124759+MohamedFarhun@users.noreply.github.com>
2023-03-08 20:53:50 +0530

    Changes to be committed: 	new file:   ._.DS_Store 	new file:   .gitignore 	new file:   .netlify/state.json 	new file:   .parcel-cache/00cdd157ba2aa58e 	new file:   .parcel-cache/3dcdc4955bf9973b 	new file:   .parcel-cache/c515a02e28e10b32.txt 	new file:   .parcel-cache/data.mdb 	new file:   .parcel-cache/lock.mdb 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/._.DS_Store 	new file:   public/._3-removebg-preview.ico 	new file:   public/._logo-dark.png 	new file:   public/._pexels.png 	new file:   public/3-removebg-preview.ico 	new file:   public/index.html 	new file:   public/logo-dark.png 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/pexels.png 	new file:   public/robots.txt 	new file:   solidityfiles/da02.sol 	new file:   src/About.js 	new file:   src/Activist.js 	new file:   src/AnnualReport.css 	new file:   src/AnnualReport.js 	new file:   src/App.css 	new file:   src/App.js 	new file:   src/Areaname.js 	new file:   src/Blogs.css 	new file:   src/Blogs.js 	new file:   src/DepositFarmer.js 	new file:   src/Farmer.js 	new file:   src/Home.js 	new file:   src/Login.js 	new file:   src/NameFarmer.js 	new file:   src/Navbar.js 	new file:   src/NavbarElements.js 	new file:   src/Owner.js 	new file:   src/Screen.js 	new file:   src/SignInFarmer.js 	new file:   src/SignInOwner.js 	new file:   src/Signin.js 	new file:   src/VoteFarmer.js 	new file:   src/abi.json 	new file:   src/about.css 	new file:   src/address.js 	new file:   src/claimActivist.js 	new file:   src/constants.js 	new file:   src/events.js 	new file:   src/farmer.css 	new file:   src/favicon.css 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/owner.css 	new file:   src/withdrawFarmer.js 	new file:   yarn.lock

---
+++ src/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+import './favicon.css';
+
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+        <NavLink to="/">
+          <img className='fav-nav' src='./3-removebg-preview.ico' />
+          </NavLink>
+        <NavLink to='/'>
+            Home 
+          </NavLink>
+          <NavLink to='/about'>
+            Connect Your Wallet
+          </NavLink>
+          <NavLink to='/annual'>
+            About project
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
